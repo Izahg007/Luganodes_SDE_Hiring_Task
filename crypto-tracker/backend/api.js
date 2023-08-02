d1d5d497212d6dbd89bbf243e7d5c7d680664ebb
Ayan Ghazi <ayan.ghazi007@gmail.com>
2023-08-02 16:53:41 +0530

    made final changes

--- crypto-tracker/backend/api.js
+++ crypto-tracker/backend/api.js
@@ -7,35 +7,49 @@ const PORT = 3001;
 
 app.use(cors());
 
-// Define your API endpoint base URL
 const BASE_URL = 'https://api.coingecko.com/api/v3';
 
-// Endpoint to get cryptocurrency prices
-app.get('/prices', async (req, res) => {
+app.get('/fetch_coin_info', async (req, res) => {
   try {
-    const { coinName } = req.query;
-
+    const { coinName, vsCurrency } = req.query;
     if (!coinName || typeof coinName !== 'string') {
       return res.status(400).json({ error: 'Invalid coin name' });
     }
 
-    const response = await axios.get(`${BASE_URL}/simple/price`, {
+    if (!vsCurrency || typeof vsCurrency !== 'string') {
+      return res.status(400).json({ error: 'Invalid vsCurrency' });
+    }
+
+    const priceResponse = await axios.get(`${BASE_URL}/simple/price`, {
       params: {
         ids: coinName,
-        vs_currencies: 'usd',
+        vs_currencies: vsCurrency
       },
     });
 
-    const price = response.data[coinName]?.usd;
+    const infoResponse = await axios.get(`${BASE_URL}/coins/markets`, {
+      params: {
+        vs_currency: vsCurrency,
+        ids: coinName,
+        order: 'market_cap_desc',
+        per_page: 1,
+        page: 1,
+        sparkline: false,
+      },
+    });
 
-    if (!price) {
+    const price = priceResponse.data[coinName]?.[vsCurrency];
+    const ticker = infoResponse.data[0]?.symbol;
+    const icon = infoResponse.data[0]?.image;
+    const currentCoin = coinName;
+    if (!price || !ticker || !icon || !currentCoin) {
       return res.status(404).json({ error: 'Coin not found' });
     }
 
-    res.json({ [coinName]: { usd: price } });
+    res.json({ coinName, price, ticker, icon, currentCoin });
   } catch (error) {
-    console.error('Error fetching prices:', error.message);
-    res.status(500).json({ error: 'Error fetching prices' });
+    console.error('Error fetching cryptocurrency information:', error.message);
+    res.status(500).json({ error: 'Error fetching cryptocurrency information' });
   }
 });
 
